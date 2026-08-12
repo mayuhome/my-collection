10368c4544961412d0486c19244622df4f3c0b1d
jade <mayuhome@163.com>
2026-08-12 10:29:51 +0800

    Initial Commit

---
+++ my-collection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-collection
+//
+//  Created by Ma Jade on 2026/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-collection/my_collectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_collectionApp.swift
+//  my-collection
+//
+//  Created by Ma Jade on 2026/8/12.
+//
+
+import SwiftUI
+
+@main
+struct my_collectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
